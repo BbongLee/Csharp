c9a7a1145efdf3b8148197dd1211811911ffed9d
BbongLee <jebon0722@gmail.com>
2018-03-06 15:18:36 +0900

    두번째 수업!!

---
+++ C01.HelloWorld/C01.HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+            Console.ReadKey(); //창이 닫히는 것을 방지!!!
+            //Console.ReadLine();
+
+            int tmpX;
+            int tmpY;
+            tmpX = 7;
+            tmpY = 8;
+
+            Console.WriteLine("%d + %d = %d", tmpX, tmpY, tmpX + tmpY);
+            Console.WriteLine("{0} + {1} = {2}", tmpX, tmpY, tmpX + tmpY);
+
+            Console.WriteLine("{0} + {1} = {2} ({0},{1})", tmpX, tmpY, tmpX + tmpY);
+            Console.WriteLine("{0} + {1} = {2},,,,,{3}", tmpX.ToString(), tmpY.ToString(),
+                tmpX.ToString() + tmpY.ToString(), (tmpX+tmpY).ToString());
+
+            string tmpName = "Class";
+            String tmpLang = "C#";
+
+            Console.WriteLine("{0} : {1}", tmpName, tmpLang);
+            Console.WriteLine("{0}", tmpName + " ; " + tmpLang);
+            Console.WriteLine("{0} : {1}", tmpName, tmpLang);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter01_02/Chapter01_02/CMyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01_02
+{
+    class CMyClass
+    {
+        public static int Add(int a, int b) //static 이 붙으면 객체를 생성하지 않고 직접호출!!
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            return (a + b);
+        }
+        public int Multiply(int a, int b)  //객체생성하기!!
+        {
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            return (a * b);
+        }
+    }
+}
